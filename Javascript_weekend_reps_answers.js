1c4cf73b4743a4066b2d618c84ddf178c3b1e4ef
Felipe G <pipe2705@gmail.com>
2019-10-19 12:12:42 -0700

    Second Commit. Answers to section  B Strings

--- Javascript_weekend_reps_answers.js
+++ Javascript_weekend_reps_answers.js
@@ -1,9 +1,42 @@
 
-console.log("hello");
+
+// B STRINGS 
+/* 
+
+1 - 
+Create a variable called firstVariable.
+Assign it the value of the string "Hello World"
+Change the value of this variable to some number.
+Store the value of firstVariable in a new variable called secondVariable
+Change the value of secondVariable to any string.
+What is the value of firstVariable?*/
 
 
-console.log("hello world");
+let firstVariable = "Hello World"; 
 
-let oldVariable = 5;
-let newVariable = oldVariable;
-console.log(newVariable);
+firstVariable = 5;
+
+let secondVariable = firstVariable;
+
+secondVariable = "This is the new value of this variable"
+
+console.log(secondVariable);
+// The value is " This is the new Value of this variable"
+
+
+/* 
+2 -
+Create a variable called yourName and set it equal to your name as a string. 
+Then, write an expression that takes the string "Hello, my name is " and the variable yourName so that it returns a new string with them concatenated.
+*/
+
+let yourName = "Felipe Gonzalez";
+
+console.log("Hello my name is " + yourName);
+
+/*
+
+C. Booleans
+Using the provided variable definitions, replace the blanks so that all log statements print true in the console. 
+Answers should be all be valid JS syntax and not weird things that don't make sense but happen to print true to the console.
+
