4700ffd53fdd5967be6348bf30bb139bc690fe05
Felipe G <pipe2705@gmail.com>
2019-10-19 19:59:17 -0700

    SECTION III D Change Values

--- Javascript_weekend_reps_answers.js
+++ Javascript_weekend_reps_answers.js
@@ -261,8 +261,20 @@ randomThings[2] = "World";
 console.log(randomThings[2]);
 //Check the value of the array to make sure it updated the array. How? Why, yes! console.log();
 
+/*
+D. Change values
+Given the following array */
 
+const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
 
+//What would you write to access the 3rd element of the array?
+ourClass[2];
+console.log(ourClass[2]);
+//Change the value of "Github" to "Octocat"
+ourClass[4] = "Octocat"
+//Add a new element, "Cloud City" to the array.
+ourClass.push("Cloud City");
+console.log(ourClass);
 
 
 
